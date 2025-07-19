b26991b2443789d8f18476c3f04c26da7e8d3fb9
shajiyaMalik <syedakshmsh@gmail.com>
2025-07-19 19:04:53 +0500

    Your message here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import Navbar from "./Component/Navbar";
 import { Hero } from "./Component/Hero";
 import Category from './Category/page';
 import Collection from './Collection/page';
 import Spoted from './Spoted/page';
 import Footer from "./Component/Footer";
-
 
 export default function Home() {
   return (
